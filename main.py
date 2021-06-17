092929ae114eae23b9ba9c28bec6e31b275a817f
Muhammad Hilmi Ramadhan <hilmi.muhammad.rmdhn@gmail.com>
2021-06-17 14:33:03 +0700

    codes awal

---
+++ main.py
@@ -0,0 +1,23 @@
+from pushbullet import Pushbullet
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN) # input raindrop sensor 
+
+
+pb = Pushbullet("o.tIdmXbgaQVMr2MHg03C39cCto0sxu9nD")
+print(pb.devices)
+
+
+while True:
+    i = GPIO.input(11) # raindrop sensor
+    if i == 0:
+        print "Gorong udan, rung waktu e entas jemuran"
+        sleep(1)
+    elif i == 1:
+        print "Arep udan, gek ndang balik entas o no jemuran Ibuk"
+
+        dev = pb.get_device('Samsung SM-G985F')
+        push = dev.push_note("Alert!!", "Someone is in your house")
+        sleep(1)
